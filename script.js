6610f761e8b5a3bb52531a665119cec8c1c3aed7
ayushrai-375 <ayushraiji1321@gmail.com>
2026-02-12 12:06:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+document.getElementById("exploreBtn").addEventListener("click", function() {
+    alert("Welcome to Accio Jobs! Start exploring opportunities 🚀");
+});
